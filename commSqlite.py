89fe12ad559df6a26bb2ae5be6bd8a1f2462c08e
snailpjw <alanwei1202@gmail.com>
2019-01-21 17:21:58 +0800

    regularly commit

--- commSqlite.py
+++ commSqlite.py
@@ -8,13 +8,19 @@ print("Opened database successfully")
 
 cursor = c.execute("SELECT date, time, ecg from Records where serialno = 23")
 
+index = 0
 for row in cursor:
-    print("ECG = ", row[2][:14])
-    print("\n")
+    while index < len(row[2]):
+        data = row[2][index] << 6 | row[2][index + 1]    
+        print(data+"\n")
+        index += 2
 
 print("Operation done successfully")
 conn.close()
 
 
+# for row in cursor:
     # print("Date = ", row[0])
     # print("Time = ", row[1])
+    # print("ECG = ", row[2][:2])
+    # print("\n")
